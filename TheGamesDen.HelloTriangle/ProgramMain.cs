19352d1d1e069c05ad3e20188271c0645f59ae3a
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-01-17 13:35:49 -0700

    REPO: Initialize repository.

---
+++ TheGamesDen.HelloTriangle/ProgramMain.cs
@@ -0,0 +1,9 @@
+﻿namespace TheGamesDen.GraphicsProgSample.HelloTriangle;
+
+public static class ProgramMain
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("hello from ProgramMain!");
+    }
+}
