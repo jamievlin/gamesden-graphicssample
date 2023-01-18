5fc610fb95b5e84a2a6920814b311fb6a787c28f
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-01-18 01:03:33 -0700

    GLSL: Add shaders to compilation target.

---
+++ TheGamesDen.HelloTriangle/Shaders/MainFragmentShader.glsl
@@ -0,0 +1,13 @@
+﻿#version 450
+
+in vec4 fragColor;
+
+// In some older OpenGL tutorials, gl_FragColor may be used instead, but is no longer recommended.
+// If there is only a single output vec4 as target, that target is assumed to be the color of the fragment.
+out vec4 outColor;
+
+void main()
+{
+    // Here, we forward the fragColor we passed from the Vertex Shader (through the rasterizer) to outColor
+    outColor = fragColor;
+}
---
+++ TheGamesDen.HelloTriangle/Shaders/MainVertexShader.glsl
@@ -0,0 +1,16 @@
+﻿#version 450
+
+in vec2 vtxInPos;
+in vec4 vtxInColor;
+
+out vec4 fragColor;
+
+void main()
+{
+    gl_Position = vec4(vtxInPos, 0.f, 1.f);
+    
+    // The Fragment shader does not have direct access to vertex attribuites
+    // (in this, vtxInColor), hence we need to forward vtxInColor as fragColor
+    // for the rasterizer, and hence the fragment shader
+    fragColor = vtxInColor;
+}
