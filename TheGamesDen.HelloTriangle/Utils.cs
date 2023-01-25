a085ae36a150058d0659bf2a0de3c9fdb42111cf
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-01-25 14:04:56 -0700

    UTILS: Add function to create shader in OpenGL.

--- TheGamesDen.HelloTriangle/Utils.cs
+++ TheGamesDen.HelloTriangle/Utils.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Resources;
+using OpenTK.Graphics.OpenGL4;
 
 namespace TheGamesDen.GraphicsProgSample.HelloTriangle;
 
@@ -16,7 +17,7 @@ public static class Utils
     /// </summary>
     /// <param name="name">Name of the shader to pass to the Resource Reader</param>
     /// <returns>Shader data as text</returns>
-    public static string LoadShaderAsText(string name)
+    private static string LoadShaderAsText(string name)
     {
         // We use EmbeddedResource for .NET Core to store and load shaders.
         using var vertexInputStream = Asm.GetManifestResourceStream($"{Asm.GetName().Name}.Shaders.{name}");
@@ -28,5 +29,41 @@ public static class Utils
         
         using var vtxReader = new StreamReader(vertexInputStream);
         return vtxReader.ReadToEnd();
+    }
+
+    /// <summary>
+    /// Helper function to create a shader in OpenGL.
+    /// Requires OpenGL context to be initialized
+    /// </summary>
+    /// <param name="name">File name of the Shader to load from Resource Assembly</param>
+    /// <param name="shaderType">Type of shader to create</param>
+    /// <returns>GL Shader Number</returns>
+    /// <exception cref="ApplicationException">Throws if shader compilation fails</exception>
+    public static int CreateShaderInOpenGl(string name, ShaderType shaderType)
+    {
+        var text = LoadShaderAsText(name);
+
+        var shaderNumber = GL.CreateShader(shaderType);
+        GL.ShaderSource(shaderNumber, text);
+        GL.CompileShader(shaderNumber);
+        
+        // Optional step, but helpful in checking that shader compilation happened successfully
+        GL.GetShader(shaderNumber, ShaderParameter.CompileStatus, out var shaderCompileStatus);
+        if (shaderCompileStatus == 0)
+        {
+            GL.GetShaderInfoLog(shaderNumber, out var shaderErrorText);
+
+            if (shaderErrorText != null)
+            {
+                Console.Error.WriteLine($"Cannot compile shader {name}!");
+                Console.Error.WriteLine("Shader compile error message:");
+                Console.Error.Write(shaderErrorText);
+                Console.Error.WriteLine("---------------------------");
+            }
+
+            throw new ApplicationException($"Cannot compile shader {name}");
+        }
+
+        return shaderNumber;
     }
 }
