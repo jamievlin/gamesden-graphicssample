2c859e456339e86b84a7fe33caec5501db051a2d
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-01-25 14:04:06 -0700

    VTX: Add struct for Vertex.

---
+++ TheGamesDen.HelloTriangle/Vertex.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK.Mathematics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace TheGamesDen.GraphicsProgSample.HelloTriangle;
+
+/// <summary>
+/// Struct for Vertex
+/// </summary>
+[Serializable, StructLayout(LayoutKind.Sequential)]
+public struct Vertex
+{
+    private readonly Vector2 _position;
+    private readonly Color4 _color;
+
+    public Vertex(Vector2 position, Color4 color)
+    {
+        _position = position;
+        _color = color;
+    }
+
+    public static unsafe int GetSize()
+    {
+        return sizeof(Vertex);
+    }
+
+    public static unsafe int GetColorByteOffset()
+    {
+        return sizeof(Vector2);
+    }
+}
