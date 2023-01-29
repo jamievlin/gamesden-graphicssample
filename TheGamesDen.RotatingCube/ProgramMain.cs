edc63f04819c89732dbb60faa7ef2736d59c4090
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-01-29 12:18:32 -0700

    CS: Add basic Rotating cube project files.

---
+++ TheGamesDen.RotatingCube/ProgramMain.cs
@@ -0,0 +1,11 @@
+﻿
+namespace TheGamesDen.GraphicsProgSample.RotatingCube;
+
+public static class ProgramMain
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        Console.Write("oh hi");
+    }
+}
