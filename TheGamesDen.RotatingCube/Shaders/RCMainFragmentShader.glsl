f6b2709787632eabc5a6fe6f052cf33a4f267b1c
Supakorn 'Jamie' Rassameemasmuang <jamievlin@outlook.com>
2023-01-29 16:22:18 -0700

    RC: Add basic shaders as part of rotating cube.

---
+++ TheGamesDen.RotatingCube/Shaders/RCMainFragmentShader.glsl
@@ -0,0 +1,10 @@
+﻿#version 450
+
+in vec3 outNormal;
+
+out vec4 outColor;
+
+void main()
+{
+    outColor = vec4(1.0f, 0.0f, 0.0f, 0.0f);
+}
---
+++ TheGamesDen.RotatingCube/Shaders/RCMainVertexShader.glsl
@@ -0,0 +1,18 @@
+﻿#version 450
+
+layout(location = 0)
+in vec3 vtxInPos;
+
+layout(location = 1)
+in vec3 vtxInNormal;
+
+out vec3 outNormal;
+
+// uniforms
+uniform mat4 projViewModelMatrix;
+
+void main()
+{
+    outNormal = vtxInNormal;
+    gl_Position = projViewModelMatrix * vec4(vtxInPos, 1.0f);
+}
